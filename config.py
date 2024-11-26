8258ba85487ecf27bb23d8734af4c093e7258674
ZainMalik <163726214+Danishzain05@users.noreply.github.com>
2024-11-26 22:30:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/THE-VIP-BOY-OP/VIP-CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+SUPPORT_GRP = "LinkZoneChat_05"
+UPDATE_CHNL = "LinkZone_05"
+OWNER_USERNAME = "LoveYouBachaa"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
